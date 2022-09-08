8203e3d05611fd20c3a20be95ca0ca36bea0b54b
muXxer <git@muxxer.de>
2022-09-08 18:57:32 +0200

    Add Enabled parameter for ratelimiting (#47)

--- core/faucet/routes.go
+++ core/faucet/routes.go
@@ -68,28 +68,30 @@ func setupRoutes(e *echo.Echo) {
 		return false
 	}
 
-	rateLimiterConfig := middleware.RateLimiterConfig{
-		Skipper: rateLimiterSkipper,
-		Store: middleware.NewRateLimiterMemoryStoreWithConfig(
-			middleware.RateLimiterMemoryStoreConfig{
-				Rate:      rate.Limit(float64(ParamsFaucet.RateLimit.MaxRequests) / ParamsFaucet.RateLimit.Period.Seconds()),
-				Burst:     ParamsFaucet.RateLimit.MaxBurst,
-				ExpiresIn: 5 * time.Minute,
-			},
-		),
-		IdentifierExtractor: func(ctx echo.Context) (string, error) {
-			id := ctx.RealIP()
+	if ParamsFaucet.RateLimit.Enabled {
+		rateLimiterConfig := middleware.RateLimiterConfig{
+			Skipper: rateLimiterSkipper,
+			Store: middleware.NewRateLimiterMemoryStoreWithConfig(
+				middleware.RateLimiterMemoryStoreConfig{
+					Rate:      rate.Limit(float64(ParamsFaucet.RateLimit.MaxRequests) / ParamsFaucet.RateLimit.Period.Seconds()),
+					Burst:     ParamsFaucet.RateLimit.MaxBurst,
+					ExpiresIn: 5 * time.Minute,
+				},
+			),
+			IdentifierExtractor: func(ctx echo.Context) (string, error) {
+				id := ctx.RealIP()
 
-			return id, nil
-		},
-		ErrorHandler: func(context echo.Context, err error) error {
-			return context.JSON(http.StatusForbidden, nil)
-		},
-		DenyHandler: func(context echo.Context, identifier string, err error) error {
-			return context.JSON(http.StatusTooManyRequests, nil)
-		},
+				return id, nil
+			},
+			ErrorHandler: func(context echo.Context, err error) error {
+				return context.JSON(http.StatusForbidden, nil)
+			},
+			DenyHandler: func(context echo.Context, identifier string, err error) error {
+				return context.JSON(http.StatusTooManyRequests, nil)
+			},
+		}
+		apiGroup.Use(middleware.RateLimiterWithConfig(rateLimiterConfig))
 	}
-	apiGroup.Use(middleware.RateLimiterWithConfig(rateLimiterConfig))
 
 	apiGroup.GET(RouteFaucetInfo, func(c echo.Context) error {
 		resp, err := getFaucetInfo(c)
