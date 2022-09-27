a14bb2494fe1f73adf870ad96bd21e5431015c90
Dr-Electron <dr-electr0n@protonmail.com>
2022-09-27 22:45:46 +0200

    Remove docs version info (#57)

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -5,18 +5,11 @@ module.exports = {
         [
             '@docusaurus/plugin-content-docs',
             {
-                id: 'inx-faucet-develop',
+                id: 'inx-faucet',
                 path: path.resolve(__dirname, 'docs'),
                 routeBasePath: 'inx-faucet',
                 sidebarPath: path.resolve(__dirname, 'sidebars.js'),
                 editUrl: 'https://github.com/iotaledger/inx-faucet/edit/develop/documentation',
-                versions: {
-                    current: {
-                        label: 'Develop',
-                        path: 'develop',
-                        badge: true
-                    },
-                },
             }
         ],
     ],
